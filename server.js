8ecb0beafaa6b4c4b654aa8bd4931263e3a51539
Luís Passos <luis.passos013@gmail.com>
2021-11-28 00:19:23 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.set("view engine", "ejs")
+app.use(express.static(__dirname + '/public'))
+
+app.get("/", (req, res) => {
+    res.render("./index.ejs")
+})
+
+app.listen(8080);
+console.log("Running 🚀")
