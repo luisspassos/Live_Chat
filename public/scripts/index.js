26a9b112b0de27a63e0762c56c9befd45d2f4cc8
Luís <luis.passos013@gmail.com>
2021-11-28 23:15:40 -0300

    message input with div with contenteditable

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,17 +1,24 @@
 const emojiList = document.querySelector(".emojiList");
 const emojis = document.querySelectorAll(".emojiList > li");
 const openEmojiListBtn = document.querySelector(".messageInput > img");
+const messageInput = document.querySelector(".messageInput > div");
 
-window.addEventListener("click", (e)=> {
-    emojis.forEach(emoji => {
-        if(e.target != openEmojiListBtn && e.target != emoji) {
-            emojiList.style.display = "none";
-        }
-    })
-    
+window.addEventListener("click", (e) => {
+    if (e.target.parentNode !== emojiList && e.target !== openEmojiListBtn) {
+        emojiList.style.display = "none"
+    }
 })
 
-openEmojiListBtn.addEventListener("click", ()=> {
-    emojiList.style.display = "flex";
+openEmojiListBtn.addEventListener("click", () => {
+    if(emojiList.style.display === "none") {
+        emojiList.style.display = "flex"
+    } else {
+        emojiList.style.display = "none"
+    }
 })
+
+function insertEmoji(emoji) {
+    messageInput.textContent += emoji;
+}
+
 
