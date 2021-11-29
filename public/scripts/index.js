bc72a0aca5713f933d90323e4a906b9394344ffd
Luís Passos <luis.passos013@gmail.com>
2021-11-29 13:17:06 -0300

    text area of messages working normally, but emoji influence the size

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,7 +1,36 @@
 const emojiList = document.querySelector(".emojiList");
 const emojis = document.querySelectorAll(".emojiList > li");
 const openEmojiListBtn = document.querySelector(".messageInput > img");
-const messageInput = document.querySelector(".messageInput > div");
+const messageTextAreaWrapper = document.querySelector(".textAreaWrapper");
+const messageTextArea = document.querySelector(".textAreaWrapper > textarea");
+
+//Textarea
+
+messageTextAreaWrapper.addEventListener("click", ()=> {
+    messageTextArea.focus();
+})
+
+messageTextArea.addEventListener("focus", ()=> {
+    messageTextAreaWrapper.style.outline = "var(--PURPLE-SECONDARY) solid 3px"
+})
+
+messageTextArea.addEventListener("blur", ()=> {
+    messageTextAreaWrapper.style.outline = "var(--PURPLE-TERTIARY) solid 3px"
+})
+
+const tx = document.getElementsByTagName("textarea");
+for (let i = 0; i < tx.length; i++) {
+  tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+  tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput() {
+  this.style.height = "auto";
+  this.style.height = (this.scrollHeight) + "px";
+}
+
+
+// emoji List
 
 window.addEventListener("click", (e) => {
     if (e.target.parentNode !== emojiList && e.target !== openEmojiListBtn) {
@@ -18,7 +47,7 @@ openEmojiListBtn.addEventListener("click", () => {
 })
 
 function insertEmoji(emoji) {
-    messageInput.textContent += emoji;
+    messageTextArea.value += emoji;
 }
 
 
