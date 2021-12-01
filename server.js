b172d90f00b4272780518f28e7b80531289858e9
Luís <luis.passos013@gmail.com>
2021-11-30 23:13:47 -0300

    added socket io to project

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+
 const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + '/public'))
@@ -8,5 +11,13 @@ app.get("/", (req, res) => {
     res.render("./index.ejs")
 })
 
-app.listen(8080);
+io.on('connection', socket => {
+    console.log(`Socket conectado: ${socket.id}`)
+
+    socket.on("sendMessage", data => {
+        console.log(data)
+    })
+})
+
+server.listen(8080);
 console.log("Running 🚀")
